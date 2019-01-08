bf88ec9552df8ce362366229da71df85b19dcf4f
Anastasia Fomina <anastasia.o.fomina@gmail.com>
2019-01-09 01:21:45 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props) 
+    this.state = {
+      imgUrl: 'https://cs4.pikabu.ru/post_img/big/2014/03/19/10/1395243232_1089834701.jpg'
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+        <button className="generateButton">
+          Get random dog
+        </button>
+        <img
+          alt='dog'
+          src={this.state.imgUrl}
+          className="dogImage"
+        >
+        </img>
       </div>
     );
   }
